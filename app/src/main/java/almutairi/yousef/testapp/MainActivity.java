9871f15e528c7d8c7c6b5d0a7ce2c7f74aadb4de
yousef991 <vip.91091@hotmail.com>
2015-11-17 16:14:50 +0900

    added samlple

--- app/src/main/java/almutairi/yousef/testapp/MainActivity.java
+++ app/src/main/java/almutairi/yousef/testapp/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         et = (EditText) findViewById(R.id.editText);
 
-
+        //////////////
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
