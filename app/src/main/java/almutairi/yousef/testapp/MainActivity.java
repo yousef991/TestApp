528d7a21116f01f6ebbe1915b82b96d8c58a786a
yousef991 <vip.91091@hotmail.com>
2015-11-17 16:11:30 +0900

    Initial commit

---
+++ app/src/main/java/almutairi/yousef/testapp/MainActivity.java
@@ -0,0 +1,30 @@
+package almutairi.yousef.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button bt;
+    EditText et;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt = (Button) this.findViewById(R.id.button);
+        bt.setText("TTTTTTTTTTTTTT");
+
+        et = (EditText) findViewById(R.id.editText);
+
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               et.setText("aaaaaaaaaaaaaaaaaaaa");
+            }
+        });
+    }
+}
